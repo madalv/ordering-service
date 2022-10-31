c9b29d4ef07b709fa47485b42cf7fc6e4bb2451d
madalv <vlada.magal@isa.utm.md>
2022-10-31 23:04:46 +0200

    avg rating in food ordering service

--- src/main/kotlin/com/madalv/plugins/Routing.kt
+++ src/main/kotlin/com/madalv/plugins/Routing.kt
@@ -10,55 +10,71 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun Application.configureRouting() {
 
-    routing {
-        get("/") {
-            call.respondText("Hello Food Ordering Service!")
-        }
-        // CLIENT SERVICES GETS RESTAURANTS MENU
-        get("/menu") {
-            call.respond(MenusData(restaurants.size, restaurants))
-        }
-        // CLIENT SENDS ORDER
-        post("/order") {
-            launch {
+routing {
+    get("/") {
+        call.respondText("Hello Food Ordering Service!")
+    }
+    // CLIENT SERVICES GETS RESTAURANTS MENU
+    get("/menu") {
+        call.respond(MenusData(restaurants.size, restaurants))
+    }
+    // CLIENT SENDS ORDER
+    post("/order") {
+        launch {
 
-                val takeoutlist: TakeoutList = call.receive()
-                val responselist = TakeoutResponseList(1, mutableListOf())
+            val takeoutlist: TakeoutList = call.receive()
+            val responselist = TakeoutResponseList(1, mutableListOf())
 
-                if (!restaurants.any()) logger.debug { "NO RESTAURANTS YET, WHAT ARE YOU ORDERING?" }
-                else for (takeout in takeoutlist.orders) {
+            if (!restaurants.any()) logger.debug { "NO RESTAURANTS YET, WHAT ARE YOU ORDERING?" }
+            else for (takeout in takeoutlist.orders) {
 
-                    val order = Order(takeout.items, takeout.priority, takeout.maxWait, takeout.createdTime)
+                val order = Order(takeout.items, takeout.priority, takeout.maxWait, takeout.createdTime)
 
-                    // run blocking is necessarry cause otherwise the call.respond() at the end has nothing to send back
-                    runBlocking {
-                        val response: TakeoutResponse = client.post("http://${restaurants[takeout.restaurantID - 1].address}/v2/order") {
-                            contentType(ContentType.Application.Json)
-                            setBody(order)
-                        }.body()
-                        println(response)
-                        responselist.responses.add(response)
-                    }
-                    logger.debug { "SENT $order TO RESTAURANT ${restaurants[takeout.restaurantID - 1].name} @ ${restaurants[takeout.restaurantID - 1].address}" }
+                // run blocking is necessarry cause otherwise the call.respond() at the end has nothing to send back
+                runBlocking {
+                    val response: TakeoutResponse = client.post("http://${restaurants[takeout.restaurantID - 1].address}/v2/order") {
+                        contentType(ContentType.Application.Json)
+                        setBody(order)
+                    }.body()
+                    println(response)
+                    responselist.responses.add(response)
                 }
-
-                call.respond(responselist)
+                logger.debug { "SENT $order TO RESTAURANT ${restaurants[takeout.restaurantID - 1].name} @ ${restaurants[takeout.restaurantID - 1].address}" }
             }
-        }
-        // RESTAURANT REGISTERS TO FOOD ORDERDING SERVICE
-        post("/register") {
-                val restaurant: RestaurantData = call.receive()
-                logger.debug { "RESTAURANT REGISTERED: $restaurant" }
-                restaurants.add(restaurant)
-        }
-        post("/rating") {
 
+            call.respond(responselist)
+        }
+    }
+    // RESTAURANT REGISTERS TO FOOD ORDERDING SERVICE
+    post("/register") {
+            val restaurant: RestaurantData = call.receive()
+            logger.debug { "RESTAURANT REGISTERED: $restaurant" }
+            restaurants.add(restaurant)
+    }
+
+    post("/rating") {
+        launch {
+            val ratings: Ratings = call.receive()
+            logger.debug { "GOT RATINGS $ratings" }
+            var currRating = 0.0
+            for (rating in ratings.ratings) {
+                val response = async {
+                    client.post("http://${restaurants[rating.restaurantId.minus(1)].address}/v2/rating") {
+                        contentType(ContentType.Application.Json)
+                        setBody(rating)
+                    }
+                }
+                currRating += response.await().body<RestaurantResponse>().resAvgRating
+                nrOrders.getAndIncrement()
+                logger.debug { "$currRating $nrOrders" }
+                ratingChannel.send(currRating / ratings.ratings.size)
+                logger.debug { "${currRating/ ratings.ratings.size}  NRORDERS $nrOrders" }
+                }
+            }
         }
     }
 }
